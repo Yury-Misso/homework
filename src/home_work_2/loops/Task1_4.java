0e4b42a900a43a409e1221ba01a3a0ceb9c005cd
Yury Misovets <VectorX69@gmail.com>
2023-04-28 23:07:02 +0300

    Task 1.4 is done

---
+++ src/home_work_2/loops/Task1_4.java
@@ -0,0 +1,78 @@
+package home_work_2.loops;
+
+import java.math.BigInteger;
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Task1_4 {
+    public static void main(String[] args) {
+        Scanner scaner = new Scanner(System.in);
+        long a = 1L;
+        long otherLong;
+        long result = a;
+        long count;
+
+        BigInteger bigInteger;
+
+
+        while (true) {
+            result = a;
+            count = 0;
+            System.out.println("long a = 1");
+
+            System.out.println("¬ведите число на которое будем умножать");
+            while (true) {
+                if (scaner.hasNextLong()) {
+                    otherLong = scaner.nextLong();
+                    scaner.nextLine();
+                    break;
+
+                } else {
+                    System.out.println("¬ведите число на которое будем умножать");
+                    scaner.nextLine();
+                    continue;
+                }
+            }
+
+            while (true) {
+                try {
+                    result = Math.multiplyExact(result, otherLong);
+                } catch (Exception e) {
+                    System.out.println("„исло до переполнени€:");
+                    System.out.println(new DecimalFormat("#,###").format(result));
+                    break;
+                }
+            }
+
+            bigInteger = BigInteger.valueOf(result).multiply(BigInteger.valueOf(otherLong));
+
+            System.out.println("„исло после переполнени€:");
+            System.out.println(getNumberInFormat(bigInteger));
+
+            System.out.println();
+
+        }
+
+    }
+
+    public static String getNumberInFormat(BigInteger bigInteger) {
+        StringBuilder stringBuilder = new StringBuilder();
+        String s = bigInteger.toString();
+        int count = 0;
+        char[] digitOfNumber = s.toCharArray();
+
+        for (int i = digitOfNumber.length - 1; i >= 0; i--) {
+            if (count < 3) {
+                stringBuilder.append(digitOfNumber[i]);
+                count++;
+            } else {
+                stringBuilder.append(" ");
+                stringBuilder.append(digitOfNumber[i]);
+                count = 1;
+            }
+        }
+        stringBuilder.reverse();
+
+        return stringBuilder.toString();
+    }
+}
