70d56896d5128967150a8b1c2905e8c2be7b3e2f
Yury-Misso <VectorX69@gmail.com>
2023-05-20 12:05:38 +0300

    Changed the encoding

--- src/home_work_1/Task61.java
+++ src/home_work_1/Task61.java
@@ -16,17 +16,17 @@ public class Task61 {
                 break;
             }
 
-            if ((name.equalsIgnoreCase("Р’Р°СЃСЏ"))
-                    || (name.equalsIgnoreCase("РђРЅР°СЃС‚Р°СЃРёСЏ"))) {
-                if (name.equalsIgnoreCase("Р’Р°СЃСЏ")) {
-                    System.out.println("РџСЂРёРІРµС‚!");
-                    System.out.println("РЇ С‚РµР±СЏ С‚Р°Рє РґРѕР»РіРѕ Р¶РґР°Р»");
+            if ((name.equalsIgnoreCase("Вася"))
+                    || (name.equalsIgnoreCase("Анастасия"))) {
+                if (name.equalsIgnoreCase("Вася")) {
+                    System.out.println("Привет!");
+                    System.out.println("Я тебя так долго ждал");
                 }
-                if (name.equalsIgnoreCase("РђРЅР°СЃС‚Р°СЃРёСЏ")) {
-                    System.out.println("РЇ С‚РµР±СЏ С‚Р°Рє РґРѕР»РіРѕ Р¶РґР°Р»");
+                if (name.equalsIgnoreCase("Анастасия")) {
+                    System.out.println("Я тебя так долго ждал");
                 }
             } else {
-                System.out.println("Р”РѕР±СЂС‹Р№ РґРµРЅСЊ, Р° РІС‹ РєС‚Рѕ?");
+                System.out.println("Добрый день, а вы кто?");
             }
 
         }
