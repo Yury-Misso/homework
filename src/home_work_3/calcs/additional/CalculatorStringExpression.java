b2d2ea20a65857b3bd2a3e2a89daa62cf83f0944
Yury-Misso <VectorX69@gmail.com>
2023-05-21 05:38:11 +0300

    CalculatorStringExpression
    is not finished

--- src/home_work_3/calcs/additional/CalculatorStringExpression.java
+++ src/home_work_3/calcs/additional/CalculatorStringExpression.java
@@ -1,7 +1,191 @@
 package home_work_3.calcs.additional;
 
+import home_work_3.calcs.api.ICalculator;
+
 public class CalculatorStringExpression {
 
+    ICalculator iCalculator;
+
+    public CalculatorStringExpression(ICalculator iCalculator) {
+        this.iCalculator = iCalculator;
+    }
+
+    public Token calculate(MySimpleList<Token> tokenList) {
+        MySimpleList<Token> simpleListToken = new MySimpleList<>();
+        boolean endList = false;
+        int numberOfToken = 0;
+        double result = 0;
+        double resultMain = 0;
+        double resultRecur = 0;
+        int countBrackets = 0;
+        while (numberOfToken < (tokenList.size())) {
+            if (tokenList.get(numberOfToken).getType().equalsIgnoreCase("number")) {
+                simpleListToken.add(tokenList.get(numberOfToken));
+
+                numberOfToken++;
+
+            } else if (tokenList.get(numberOfToken).getType().equalsIgnoreCase("function")) {
+                if ((tokenList.get(numberOfToken).getValue().equalsIgnoreCase("+"))
+                        || (tokenList.get(numberOfToken).getValue().equalsIgnoreCase("-"))
+                        || (tokenList.get(numberOfToken).getValue().equalsIgnoreCase("*"))
+                        || (tokenList.get(numberOfToken).getValue().equalsIgnoreCase("/"))) {
+                    simpleListToken.add(tokenList.get(numberOfToken));
+
+                    numberOfToken++;
+
+                }
+                if (tokenList.get(numberOfToken).getValue().equalsIgnoreCase("(")) {
+                    MySimpleList<Token> innerListToken = new MySimpleList<>();
+                    countBrackets++;
+                    //innerListToken.add(tokenList.get(numberOfToken));
+                    numberOfToken++;
+
+                    while (countBrackets != 0) {
+
+                        if (tokenList.get(numberOfToken).getValue().equalsIgnoreCase("(")) {
+                            countBrackets++;
+
+                        }
+                        if (tokenList.get(numberOfToken).getValue().equalsIgnoreCase(")")) {
+                            countBrackets--;
+
+                        }
+                        innerListToken.add(tokenList.get(numberOfToken));
+                        numberOfToken++;
+
+                    }
+                    innerListToken.remove(innerListToken.size() - 1);
+                    System.out.println(innerListToken);
+
+                    simpleListToken.add(new Token("number", calculate(innerListToken).getValue()));
+                }
+
+            }
+
+        }
+
+        return new Token("number", Double.toString(calculateSimple(simpleListToken)));
+    }
+
+
+    public double calculateSimple(MySimpleList<Token> tokenList) {
+        int maxPriority = getPriority(tokenList);
+
+        double firstNumber = 0;
+        double secondNumber = 0;
+        double result = 0;
+
+        if (tokenList.size() == 2) {
+            if ((tokenList.get(0).getValue().equalsIgnoreCase("-")) &&
+                    (tokenList.get(1).getType().equalsIgnoreCase("number"))) {
+                secondNumber = Double.parseDouble(tokenList.get(1).getValue());
+                result = firstNumber - secondNumber;
+            }
+            return result;
+        }
+
+
+        if (tokenList.size > 2) {
+            int numberOfToken = 0;
+            while (tokenList.size() > 1) {
+
+                while (maxPriority == 2) {
+
+                    while (!((tokenList.get(numberOfToken).getType().equalsIgnoreCase("function"))
+                            && ((tokenList.get(numberOfToken).getValue().equalsIgnoreCase("*"))
+                            || (tokenList.get(numberOfToken).getValue().equalsIgnoreCase("/"))))) {
+                        numberOfToken++;
+                    }
+
+                    if (tokenList.get(numberOfToken).getValue().equalsIgnoreCase("*")) {
+                        firstNumber = Double.parseDouble(tokenList.get(numberOfToken - 1).getValue());
+
+                        secondNumber = Double.parseDouble(tokenList.get(numberOfToken + 1).getValue());
+
+                        result = iCalculator.multiply(firstNumber, secondNumber);
+
+                    } else if (tokenList.get(numberOfToken).getValue().equalsIgnoreCase("/")) {
+                        firstNumber = Double.parseDouble(tokenList.get(numberOfToken - 1).getValue());
+                        secondNumber = Double.parseDouble(tokenList.get(numberOfToken + 1).getValue());
+                        result = iCalculator.divide(firstNumber, secondNumber);
+                    }
+                    tokenList.remove(numberOfToken);
+                    tokenList.remove(numberOfToken);
+                    tokenList.remove(numberOfToken - 1);
+                    tokenList.add(numberOfToken - 1, new Token("number", Double.toString(result)));
+                    numberOfToken = 0;
+
+                    maxPriority = getPriority(tokenList);
+
+
+                    if (tokenList.size() == 1) {
+                        return result;
+                    }
+                }
+
+                while (maxPriority == 1) {
+
+                    while (!((tokenList.get(numberOfToken).getType().equalsIgnoreCase("function"))
+                            && ((tokenList.get(numberOfToken).getValue().equalsIgnoreCase("+"))
+                            || (tokenList.get(numberOfToken).getValue().equalsIgnoreCase("-"))))) {
+                        numberOfToken++;
+                    }
+
+                    if (tokenList.get(numberOfToken).getValue().equalsIgnoreCase("+")) {
+                        firstNumber = Double.parseDouble(tokenList.get(numberOfToken - 1).getValue());
+
+                        secondNumber = Double.parseDouble(tokenList.get(numberOfToken + 1).getValue());
+
+                        result = iCalculator.sum(firstNumber, secondNumber);
+
+                    }
+                    if (tokenList.get(numberOfToken).getValue().equalsIgnoreCase("-")) {
+                        firstNumber = Double.parseDouble(tokenList.get(numberOfToken - 1).getValue());
+                        secondNumber = Double.parseDouble(tokenList.get(numberOfToken + 1).getValue());
+                        result = iCalculator.minus(firstNumber, secondNumber);
+                    }
+                    tokenList.remove(numberOfToken);
+                    tokenList.remove(numberOfToken);
+                    tokenList.remove(numberOfToken - 1);
+
+
+                    tokenList.add(numberOfToken - 1, new Token("number", Double.toString(result)));
+
+
+                    numberOfToken = 0;
+                    maxPriority = getPriority(tokenList);
+                    if (tokenList.size() == 1) {
+
+                        return result;
+                    }
+                }
+
+            }
+
+        }
+        return 0;
+    }
+
+    private int getPriority(MySimpleList<Token> tokenList) {
+        int maxPriority = 0;
+        if (tokenList.size > 2) {
+            for (int i = 0; i < tokenList.size; i++) {
+                if (tokenList.get(i).getType().equalsIgnoreCase("function")) {
+                    if (((tokenList.get(i).getValue().equalsIgnoreCase("-")) ||
+                            (tokenList.get(i).getValue().equalsIgnoreCase("+"))) &&
+                            maxPriority < 2) {
+                        maxPriority = 1;
+                    }
+                    if ((tokenList.get(i).getValue().equalsIgnoreCase("*")) ||
+                            (tokenList.get(i).getValue().equalsIgnoreCase("/"))) {
+                        maxPriority = 2;
+                    }
+
+                }
+            }
+        }
+        return maxPriority;
+    }
 
     public MySimpleList<Token> tokenize(String s) {
         MySimpleList<Token> tokenList = new MySimpleList<>();
@@ -14,25 +198,186 @@ public class CalculatorStringExpression {
 
             for (int i = 0; i < chars.length; i++) {
                 if (chars[i] == '+') {
+                    if (stringBuilder.toString().equalsIgnoreCase("E")) {
+                        tokenList.add(new Token("number", Double.toString(2.718281828459045)));
+                        stringBuilder.setLength(0);
+                        //System.out.println("build E");
+                    }
+                    if (stringBuilder.toString().equalsIgnoreCase("Pi")) {
+                        tokenList.add(new Token("number", Double.toString(3.1415926535897)));
+                        stringBuilder.setLength(0);
+                        //System.out.println("build Pi");
+                    }
+                    if (tokenizeDigit) {
+                        tokenList.add(new Token("number", stringBuilder.toString()));
+                        //System.out.println("build number");
+                        stringBuilder.setLength(0);
+                        tokenizeDigit = false;
+                    }
                     tokenList.add(new Token("function", "+"));
+
                 } else if (chars[i] == '-') {
+                    if (stringBuilder.toString().equalsIgnoreCase("E")) {
+                        tokenList.add(new Token("E", stringBuilder.toString()));
+                        stringBuilder.setLength(0);
+                        //System.out.println("build E");
+                    }
+                    if (stringBuilder.toString().equalsIgnoreCase("Pi")) {
+                        tokenList.add(new Token("Pi", stringBuilder.toString()));
+                        stringBuilder.setLength(0);
+                        //System.out.println("build Pi");
+                    }
+                    if (tokenizeDigit) {
+                        tokenList.add(new Token("number", stringBuilder.toString()));
+                        //System.out.println("build number");
+                        stringBuilder.setLength(0);
+                        tokenizeDigit = false;
+                    }
                     tokenList.add(new Token("function", "-"));
+
                 } else if (chars[i] == '/') {
+                    if (stringBuilder.toString().equalsIgnoreCase("E")) {
+                        tokenList.add(new Token("E", stringBuilder.toString()));
+                        stringBuilder.setLength(0);
+                        //System.out.println("build E");
+                    }
+                    if (stringBuilder.toString().equalsIgnoreCase("Pi")) {
+                        tokenList.add(new Token("Pi", stringBuilder.toString()));
+                        stringBuilder.setLength(0);
+                        //System.out.println("build Pi");
+                    }
+                    if (tokenizeDigit) {
+                        tokenList.add(new Token("number", stringBuilder.toString()));
+                        //System.out.println("build number");
+                        stringBuilder.setLength(0);
+                        tokenizeDigit = false;
+                    }
                     tokenList.add(new Token("function", "/"));
+
                 } else if (chars[i] == '*') {
+                    if (stringBuilder.toString().equalsIgnoreCase("E")) {
+                        tokenList.add(new Token("E", stringBuilder.toString()));
+                        stringBuilder.setLength(0);
+                        //System.out.println("build E");
+                    }
+                    if (stringBuilder.toString().equalsIgnoreCase("Pi")) {
+                        tokenList.add(new Token("Pi", stringBuilder.toString()));
+                        stringBuilder.setLength(0);
+                        //System.out.println("build Pi");
+                    }
+                    if (tokenizeDigit) {
+                        tokenList.add(new Token("number", stringBuilder.toString()));
+                        //System.out.println("build number");
+                        stringBuilder.setLength(0);
+                        tokenizeDigit = false;
+                    }
                     tokenList.add(new Token("function", "*"));
-                } else if (chars[i] >= 48 && chars[i] <= 57 && chars[i] == '.') {
+                } else if (chars[i] == '^') {
+                    if (stringBuilder.toString().equalsIgnoreCase("E")) {
+                        tokenList.add(new Token("E", stringBuilder.toString()));
+                        stringBuilder.setLength(0);
+                        //System.out.println("build E");
+                    }
+                    if (stringBuilder.toString().equalsIgnoreCase("Pi")) {
+                        tokenList.add(new Token("Pi", stringBuilder.toString()));
+                        stringBuilder.setLength(0);
+                        //System.out.println("build Pi");
+                    }
+                    if (tokenizeDigit) {
+                        tokenList.add(new Token("number", stringBuilder.toString()));
+                        //System.out.println("build number");
+                        stringBuilder.setLength(0);
+                        tokenizeDigit = false;
+                    }
+                    tokenList.add(new Token("function", "^"));
+
+                } else if (chars[i] == '|') {
+                    if (stringBuilder.toString().equalsIgnoreCase("E")) {
+                        tokenList.add(new Token("E", stringBuilder.toString()));
+                        stringBuilder.setLength(0);
+                        //System.out.println("build E");
+                    }
+                    if (stringBuilder.toString().equalsIgnoreCase("Pi")) {
+                        tokenList.add(new Token("Pi", stringBuilder.toString()));
+                        stringBuilder.setLength(0);
+                        //System.out.println("build Pi");
+                    }
+                    if (tokenizeDigit) {
+                        tokenList.add(new Token("number", stringBuilder.toString()));
+                        //System.out.println("build number");
+                        stringBuilder.setLength(0);
+                        tokenizeDigit = false;
+                    }
+                    tokenList.add(new Token("function", "|"));
+
+                } else if (chars[i] == '(') {
+                    if (stringBuilder.toString().equalsIgnoreCase("E")) {
+                        tokenList.add(new Token("E", stringBuilder.toString()));
+                        stringBuilder.setLength(0);
+                        //System.out.println("build E");
+                    }
+                    if (stringBuilder.toString().equalsIgnoreCase("Pi")) {
+                        tokenList.add(new Token("Pi", stringBuilder.toString()));
+                        stringBuilder.setLength(0);
+                        //System.out.println("build Pi");
+                    }
+                    if (tokenizeDigit) {
+                        tokenList.add(new Token("number", stringBuilder.toString()));
+                        //System.out.println("build number");
+                        stringBuilder.setLength(0);
+                        tokenizeDigit = false;
+                    }
+                    tokenList.add(new Token("function", "("));
+
+                } else if (chars[i] == ')') {
+                    if (stringBuilder.toString().equalsIgnoreCase("E")) {
+                        tokenList.add(new Token("E", stringBuilder.toString()));
+                        stringBuilder.setLength(0);
+                        //System.out.println("build E");
+                    }
+                    if (stringBuilder.toString().equalsIgnoreCase("Pi")) {
+                        tokenList.add(new Token("Pi", stringBuilder.toString()));
+                        stringBuilder.setLength(0);
+                        //System.out.println("build Pi");
+                    }
+                    if (tokenizeDigit) {
+                        tokenList.add(new Token("number", stringBuilder.toString()));
+                        //System.out.println("build number");
+                        stringBuilder.setLength(0);
+                        tokenizeDigit = false;
+                    }
+                    tokenList.add(new Token("function", ")"));
+
+                } else if ((chars[i] > 47 && chars[i] < 58) || chars[i] == '.') {
                     tokenizeDigit = true;
+                    stringBuilder.append(chars[i]);
+                } else if (chars[i] == 80 || chars[i] == 112 || chars[i] == 73 || chars[i] == 105) {
+                    stringBuilder.append(chars[i]);
+                } else if (chars[i] == 'E') {
+                    stringBuilder.append(chars[i]);
+                    //System.out.println(stringBuilder);
                 }
 
-                if (tokenizeDigit) {
-                    stringBuilder.append(chars[i]);
-                }
+
             }
 
+            if (stringBuilder.toString().equalsIgnoreCase("E")) {
+                tokenList.add(new Token("E", stringBuilder.toString()));
+                stringBuilder.setLength(0);
+                //System.out.println("build E");
+            }
+            if (stringBuilder.toString().equalsIgnoreCase("Pi")) {
+                tokenList.add(new Token("Pi", stringBuilder.toString()));
+                stringBuilder.setLength(0);
+                //System.out.println("build Pi");
+            }
+            if (tokenizeDigit) {
+                tokenList.add(new Token("number", stringBuilder.toString()));
+                //System.out.println("build number");
+                stringBuilder.setLength(0);
+            }
 
         }
-
 
         return tokenList;
     }
@@ -155,6 +500,37 @@ public class CalculatorStringExpression {
 
         public MySimpleList() {
             this.values = new Object[10];
+        }
+
+        public void remove(int index) {
+            if (index >= 0 && index < size) {
+                Object[] newArr = new Object[size - 1];
+                for (int j = 0; j < index; j++) {
+                    newArr[j] = values[j];
+                }
+                for (int j = index; j < size - 1; j++) {
+                    newArr[j] = values[j + 1];
+                }
+                size--;
+                values = newArr;
+            }
+        }
+
+        public void add(int i, T value) {
+            if (size == 0) {
+                add(value);
+            } else if (i >= 0 && i < size) {
+                Object[] newArr = new Object[size + 1];
+                for (int j = 0; j < i; j++) {
+                    newArr[j] = values[j];
+                }
+                newArr[i] = value;
+                for (int j = i; j < size; j++) {
+                    newArr[j + 1] = values[j];
+                }
+                size++;
+                values = newArr;
+            }
         }
 
         public void add(T value) {
--- src/home_work_3/runners/CalculatorStringExpressionMain.java
+++ src/home_work_3/runners/CalculatorStringExpressionMain.java
@@ -1,15 +1,32 @@
 package home_work_3.runners;
 
 import home_work_3.calcs.additional.CalculatorStringExpression;
-
-import java.util.Scanner;
+import home_work_3.calcs.simple.CalculatorWithMathCopy;
 
 public class CalculatorStringExpressionMain {
     public static void main(String[] args) {
-        CalculatorStringExpression calculatorStringExpression = new CalculatorStringExpression();
+        CalculatorStringExpression calculatorStringExpression = new CalculatorStringExpression(new CalculatorWithMathCopy());
+//
+        //String expression = "2.2 + | - 3 | * 4 ^ 2 + ( ( 3 - 4 ) ^ 4 / 4 ) - Pi + E";
+//
+        //System.out.println(expression);
+        //System.out.println(calculatorStringExpression.tokenize(expression));
 
-        String expression = "2.2 + | -3 | * 4 ^ 2 + ( ( 3 - 4 ) / 4 ) - Pi + E";
+       // String expression2 = " ((2 - 3)  * 4)  +  3 - 4 ";
+        String expression2 = " (2 - 3)  * (4  +  (3 - 4 ))";
+        System.out.println(expression2);
+        System.out.println(calculatorStringExpression.calculate(calculatorStringExpression.tokenize(expression2)));
 
-        System.out.println(calculatorStringExpression.tokenize(expression));
+        //System.out.println();
+        //String expression3 = " -3";
+        //System.out.println(expression3);
+        //System.out.println(calculatorStringExpression.calculateSimple(calculatorStringExpression.tokenize(expression3)));
+//
+        //System.out.println();
+        //String expression4 = " 4/2 * 5 /10";
+        //System.out.println(expression4);
+        //System.out.println(calculatorStringExpression.calculateSimple(calculatorStringExpression.tokenize(expression4)));
+
+
     }
 }
