ac1ac63971874fca1533403592dff6337db94662
Yury-Misso <VectorX69@gmail.com>
2023-05-31 19:30:26 +0300

    HomeWork5 is done
    Created DataContainer class
    implemented methods:
    - add
    - get
    - getItems
    - delete
    - void sort(Comparator<> comparator)
    - static void sort(DataContainer<> container)
    - static void sort(DataContainer<> container, Comparator<> comparator)
    Iterable interface

---
+++ tests/home_work_5/DataContainerTest.java
@@ -0,0 +1,18 @@
+package home_work_5;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class DataContainerTest {
+
+    private static DataContainer<Integer> dataContainerTtst1 = new DataContainer<>(new Integer[0]);
+
+    @Test
+    public void addMethodTest1() {
+
+    }
+
+
+
+
+}
